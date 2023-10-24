c631926b337d2bcd9c2b062393bba043d233b885
chezmarcbrown <marc@chezbrown.org>
2023-10-23 16:22:39 -0800

    checkpoingt

--- flights/admin.py
+++ flights/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Airport, Flight
 
 admin.site.register(Airport)
-admin.site.register(Flight)
+#admin.site.register(Flight)
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("origin", "destination", "duration")
+
+admin.site.register(Flight, FlightAdmin)
